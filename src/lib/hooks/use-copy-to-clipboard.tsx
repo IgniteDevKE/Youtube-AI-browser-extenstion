96fad534a9e10b7a6932a9ed2469e8095d0fc77b
Ian Chege <ianchege001@gmail.com>
2024-07-04 16:41:40 +0300

    feat: Add useCopyToClipboard hook for copying text to clipboard

---
+++ src/lib/hooks/use-copy-to-clipboard.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+export interface useCopyToClipboardProps {
+  timeout?: number
+}
+
+export const useCopyToClipboard = ({
+  timeout = 2000
+}: useCopyToClipboardProps) => {
+  const [isCopied, setIsCopied] = useState<boolean>(false)
+
+  const copyToClipboard = (value: string) => {
+    if (typeof window === "undefined" || !navigator.clipboard?.writeText) {
+      return
+    }
+    if (!value) {
+      return
+    }
+
+    navigator.clipboard.writeText(value).then(() => {
+      setIsCopied(true)
+      setTimeout(() => setIsCopied(false), timeout)
+    })
+  }
+
+  return { isCopied, copyToClipboard }
+}
