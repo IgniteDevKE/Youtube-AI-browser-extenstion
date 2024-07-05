b6888e3123650249a61247913ddac5aa7a97c9a7
Ian Chege <ianchege001@gmail.com>
2024-07-05 20:16:22 +0300

    feat: Add SummaryContext and SummaryProvider components for managing summary state

---
+++ src/contexts/summary-context.tsx
@@ -0,0 +1,101 @@
+import { createContext, useContext, useEffect, useState } from "react"
+
+import { usePort } from "@plasmohq/messaging/hook"
+
+import { models, prompts, type Model, type Prompt } from "~lib/constants"
+
+import { useExtesion } from "./extension-context"
+
+interface SummaryContext {
+  summaryModel: Model
+  setSummaryModel: (model: Model) => void
+  summaryPrompt: Prompt
+  setSummaryPrompt: (prompt: Prompt) => void
+  summaryContent: string | null
+  setSummaryContent: (content: string | null) => void
+  summaryIsError: boolean
+  setSummaryIsError: (isError: boolean) => void
+  summaryIsGenerating: boolean
+  setSummaryIsGenerating: (isGenerating: boolean) => void
+  generateSummary: (e: any) => void
+}
+
+const SummaryContext = createContext<SummaryContext | undefined>(undefined)
+
+export const useSummary = () => {
+  const context = useContext(SummaryContext)
+  if (!context) {
+    throw new Error("useSummary must be used within a SummaryProvider")
+  }
+  return context
+}
+
+interface SummaryProviderProps {
+  children: React.ReactNode
+}
+
+export const SummaryProvider = ({ children }: SummaryProviderProps) => {
+  const [summaryModel, setSummaryModel] = useState<Model>(models[0])
+  const [summaryPrompt, setSummaryPrompt] = useState<Prompt>(prompts[0])
+  const [summaryContent, setSummaryContent] = useState<string | null>(null)
+  const [summaryIsError, setSummaryIsError] = useState<boolean>(false)
+  const [summaryIsGenerating, setSummaryIsGenerating] = useState<boolean>(false)
+
+  const port = usePort("completion")
+  const { extensionData, extensionLoading } = useExtesion()
+
+  const generateSummary = async (e: any) => {
+    e.preventDefault()
+    if (summaryContent !== null) {
+      setSummaryContent(null)
+    }
+    setSummaryIsGenerating(true)
+    setSummaryIsError(false)
+    port.send({
+      prompt: summaryPrompt.content,
+      model: summaryModel.content,
+      context: extensionData
+    })
+  }
+
+  useEffect(() => {
+    setSummaryContent(null)
+    setSummaryIsGenerating(false)
+    setSummaryIsError(false)
+  }, [extensionLoading])
+
+  useEffect(() => {
+    if (port.data?.message !== undefined && port.data.isEnd === false) {
+      setSummaryContent(port.data.message)
+    } else {
+      setSummaryIsGenerating(false)
+    }
+    setSummaryIsError(false)
+  }, [port.data?.message])
+
+  useEffect(() => {
+    if (port.data?.error !== undefined && port.data?.error !== null) {
+      setSummaryIsError(true)
+      setSummaryContent(null)
+    } else {
+      setSummaryIsError(false)
+    }
+  }, [port.data?.error])
+
+  const value = {
+    summaryModel,
+    setSummaryModel,
+    summaryPrompt,
+    setSummaryPrompt,
+    summaryContent,
+    setSummaryContent,
+    summaryIsError,
+    setSummaryIsError,
+    summaryIsGenerating,
+    setSummaryIsGenerating,
+    generateSummary
+  }
+  return (
+    <SummaryContext.Provider value={value}>{children}</SummaryContext.Provider>
+  )
+}
